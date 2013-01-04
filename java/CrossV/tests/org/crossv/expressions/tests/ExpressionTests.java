74f4661757784bd808113961baba61ae2b3df69a
yochanan.miykael <yochanan.miykael@gmail.com>
2013-01-04 17:06:04 +0200

    moved tests around

--- java/CrossV/tests/org/crossv/expressions/tests/ExpressionTests.java
+++ java/CrossV/tests/org/crossv/expressions/tests/ExpressionTests.java
@@ -1,32 +1,41 @@
 package org.crossv.expressions.tests;
 
+import static org.crossv.expressions.Expression.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import org.crossv.expressions.Constant;
+import org.crossv.expressions.Expression;
+import org.crossv.expressions.IllegalOperandException;
 import org.junit.Test;
 
 public class ExpressionTests {
-	@Test
-	public void Message_toString_returnsMessage() {
-		Constant e;
-		Object value;
 
-		value = "blah";
-		e = new Constant(value);
-		assertThat(e.getValue(), is(value));
+	@Test(expected = IllegalOperandException.class)
+	public void createAndExpression_nonBooleanOperands_IllegalOperandExceptionIsThrown() {
+		and(1, 2);
 	}
 
-	// @Test
-	// public void ContidionExpression_() {
-	// If e;
-	// e = new If(condition, then, _else);
-	// }
+	@Test
+	public void createAndExpression_callingToString_getsJavaLikeExpression() {
+		Expression e = and(true, false);
+		assertThat(e.toString(), is("(true && false)"));
+	}
 
-	// @Test
-	// public void BinaryOperation_() {
-	// BinaryOperation o;
-	// o = new BinaryOperation()
-	// }
+	@Test(expected = IllegalOperandException.class)
+	public void createEqualExpression_DifferentClassOperands_IllegalOperandExceptionIsThrown() {
+		equal(1, "2");
+	}
 
+	@Test
+	public void createEqualExpression_callingToString_getsJavaLikeExpression() {
+		Expression e = equal(1, 2);
+		assertThat(e.toString(), is("(1 == 2)"));
+	}
+
+	@Test
+	public void combiningAndAndEqualExpression_callingToString_getsJavaLikeExpression() {
+		Expression e = and(false, and(equal(1, 2), equal("a", "b")));
+		assertThat(e.toString(),
+				is("(false && ((1 == 2) && (\"a\" == \"b\")))"));
+	}
 }
