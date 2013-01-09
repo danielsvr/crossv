44ae7d2d6be112c6dba11b8ecc7a58dfe7b70210
yochanan.miykael <yochanan.miykael@gmail.com>
2013-01-09 18:46:17 +0200

    DUMB COMMIT

---
+++ java/CrossV/tests/org/crossv/primitives/tests/ClassDescriptorTests.java
@@ -0,0 +1,57 @@
+package org.crossv.primitives.tests;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.crossv.primitives.ClassDescriptor;
+import org.junit.Test;
+
+public class ClassDescriptorTests {
+	@Test
+	public void executeParameterlessVoidMethod_returnsNull() throws Exception {
+		try {
+			ClassDescriptor<String> descriptor;
+			Object result;
+
+			descriptor = new ClassDescriptor<String>(String.class);
+			result = descriptor.execute("test", "wait");
+			assertThat(result, is(equalTo(null)));
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+	}
+
+	@Test(expected = NoSuchMethodException.class)
+	public void executeUndefinedMethod_throwsNoSuchMethodException()
+			throws Exception {
+		ClassDescriptor<String> descriptor;
+
+		descriptor = new ClassDescriptor<String>(String.class);
+		descriptor.execute("test", "blah");
+	}
+
+	@Test
+	public void executeEqualsOnStringPassingInt_returnsBoolean()
+			throws Exception {
+		ClassDescriptor<String> descriptor;
+		Object result;
+
+		descriptor = new ClassDescriptor<String>(String.class);
+		result = descriptor.execute("test", "equals", 123);
+
+		assertThat(result, is(Boolean.class));
+	}
+
+	@Test
+	public void executeEqualsOnStringPassingInt_returnsFalse() throws Exception {
+		ClassDescriptor<String> descriptor;
+		Object result;
+
+		descriptor = new ClassDescriptor<String>(String.class);
+		result = descriptor.execute("test", "equals", 123);
+
+		assertThat(((Boolean) result).booleanValue(), is(equalTo(false)));
+	}
+}
