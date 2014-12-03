b2b77d4b42ce40c43a03b93aee4f511103ff9bea
yochanan.miykael <yochanan.miykael@gmail.com>
2014-12-03 18:27:37 +0200

    tests for parsing context expression

--- java/CrossV/src/org/crossv/expressions/Context.java
+++ java/CrossV/src/org/crossv/expressions/Context.java
@@ -2,6 +2,9 @@ package org.crossv.expressions;
 
 import static org.crossv.primitives.ClassDescriptor.CObject;
 
+import org.crossv.parsing.grammars.antlr4.CrossVParser;
+import org.crossv.parsing.grammars.antlr4.CrossVParser.TermContext;
+
 public final class Context extends Expression {
 
 	private final Class<?> clazz;
@@ -23,9 +26,15 @@ public final class Context extends Expression {
 	public void accept(ExpressionVisitor visitor) {
 		visitor.visitContext(this);
 	}
-	
+
 	@Override
 	protected boolean isKnownOnlyAtRuntime() {
 		return true;
+	}
+
+	public static Context parse(String text) {
+		CrossVParser parser = createTextParser(text);
+		TermContext context = parser.term();
+		return (Context) context.result;
 	}
 }
--- java/CrossV/tests/org/crossv/expressions/tests/ContextExpressionTests.java
+++ java/CrossV/tests/org/crossv/expressions/tests/ContextExpressionTests.java
@@ -2,8 +2,11 @@ package org.crossv.expressions.tests;
 
 import static org.crossv.expressions.Expressions.context;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
+import org.crossv.expressions.Context;
 import org.crossv.expressions.EvaluationException;
 import org.crossv.expressions.Expression;
 import org.crossv.tests.helpers.TestObjectFactory;
@@ -18,6 +21,19 @@ public class ContextExpressionTests {
 		Expression e = context();
 		Object evaluation = e.evaluate(null, contextIntance);
 		assertThat(evaluation, is(contextIntance));
+	}
+
+	@Test
+	public void contextExpression_ToString_ReturnsJavaLikeExpression()
+			throws Exception {
+		Expression e = context();
+		assertThat(e.toString(), is("context"));
+	}
+
+	@Test
+	public void parsContextExpression_Context_ReturnsContext() throws Exception {
+		Context context = Context.parse("context");
+		assertThat(context, is(not(nullValue())));
 	}
 
 	@Test(expected = EvaluationException.class)
